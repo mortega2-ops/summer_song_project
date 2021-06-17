35ef3204bdd625ebf90d35c68d3dccc72103fe8b
maortega2-ops <maortega2@catalyte.io>
2021-06-17 11:11:20 -0600

    Scraping nationaltoday for todays holidays

--- summer_song_project/summer_song_app/views.py
+++ summer_song_project/summer_song_app/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, "summer_song_app/home.html")
+    holiday_list = []
+
+    try:
+        r = requests.get('https://nationaltoday.com/what-is-today/')
+        soup = BeautifulSoup(r.content, "lxml")
+        what_is_today = soup.find("h2", class_="entry-title").text
+        other_holidays_today = soup.find_all("p", class_="holiday-title")
+
+        holiday_list.append(what_is_today)
+        for day in other_holidays_today:
+            holiday_list.append(day.text)
+
+        today_dict = {
+            'todays_holidays': holiday_list,
+            'today': date.today()
+        }
+
+        return render(request, "summer_song_app/home.html", today_dict)
+
+    except Exception as e:
+        print('The scraping job failed. See exception: ' + str(e))
+        return HttpResponse('The scraping job failed. See exception: ' + str(e))
+
+
+
+
