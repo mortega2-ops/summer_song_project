830dab484cf4e82589788a204da3789353c2ec12
maortega2-ops <maortega2@catalyte.io>
2021-06-25 16:52:35 -0600

    Creates a List of songs based on today's holidays

--- summer_song_project/summer_song_app/views.py
+++ summer_song_project/summer_song_app/views.py
@@ -5,11 +5,15 @@ from bs4 import BeautifulSoup
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import re
 import json
 
 
 def home(request):
     holiday_list = []
+    holiday_key_words = []
+    remove_words_list = ['Global', 'International', 'National', 'Day', 'Republic', 'Audacity to', 'of',
+                         'Stand for', 'Wear a', 'I Love My', 'Awareness', 'Holiday', 'of the', 'World', 'First']
 
     try:
         r = requests.get('https://nationaltoday.com/what-is-today/')
@@ -26,6 +30,15 @@ def home(request):
             'today': date.today()
         }
 
+        holiday_regex = re.compile('|'.join(map(re.escape, remove_words_list)))
+        for holiday in holiday_list:
+            edited_holiday = holiday_regex.sub('', holiday)
+            edited_holiday = edited_holiday.strip()
+            holiday_key_words.append(edited_holiday)
+
+        request.session['today'] = str(today_dict['today'])
+        request.session['song_topics'] = holiday_key_words
+
         return render(request, "summer_song_app/home.html", today_dict)
 
     except Exception as e:
@@ -34,40 +47,30 @@ def home(request):
 
 
 def playlist_gen(request):
+    playlist = []
     recommended_song_list = []
 
     spotify = spotipy.Spotify(
         client_credentials_manager=SpotifyClientCredentials(client_id='a25f7890ec934e0c9f9fb86b7bc00cd7',
                                                             client_secret='d9b515dbfa074cd9a358db90609c0567'))
 
-    results = spotify.search(q='Eat+Your+Vegetables', limit=1, type='track', market=None)
-    # print(json.dumps(results, sort_keys=True, indent=4))
-    # print(type(results))
-    # print(json.dumps(results['tracks']['items'][0]['id'], sort_keys=True, indent=2))
+    for key_word in request.session['song_topics']:
+        playlist.append(spotify.search(q=key_word, limit=1, type='track', market=None))
+        print(json.dumps(playlist, indent=4))
+        # try:
+        #     track_id = [results['tracks']['items'][0]['id']]
+        #     playlist.append(spotify.recommendations(seed_tracks=track_id, limit=3))
+        # except IndexError:
+        #     print(f"{key_word} doesn't exist in Spotify.  Skipped.")
 
-    track_id = [results['tracks']['items'][0]['id']]
-    playlist = spotify.recommendations(seed_tracks=track_id, limit=10)
-
-    for song in playlist['tracks']:
-        print(song['name'])
-        print(song['external_urls']['spotify'])
-        recommended_song_list.append([song['name'], song['external_urls']['spotify']])
+    for i in range(len(playlist)):
+        for song in playlist[i]['tracks']['items']:
+            print(song['name'])
+            print(song['external_urls']['spotify'])
+            recommended_song_list.append([song['name'], song['external_urls']['spotify']])
 
     song_dict = {
-        'spotify_url': results['tracks']['items'][0]['external_urls']['spotify'],
-        'song_name': results['tracks']['items'][0]['name'],
         'playlist': recommended_song_list
     }
 
-    # print(json.dumps(playlist, sort_keys=True, indent=4))
-    # print(playlist['tracks'][0]['name'])
-
-
-
-
-
     return render(request, 'summer_song_app/playlist.html', song_dict)
-
-
-
-
